a6953329664c6c226f02e9a36f31c5d083e2981b
AMIRADI2006 <amirabadi2006@gmail.com>
2025-10-11 22:55:29 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// let username=prompt('please enter your username:');
+//         console.log(username);
+
+// let isadmin=confirm('Are you admin?')
+// console.log(isadmin);
+
+// alert('Hello welcome back')
+
+let number1=+prompt('pease enter number1:')
+let number2=+prompt('pease enter number2:')
+
+
+let plus=number1+number2;
+let mines=number1-number2;
+let zarb=number1*number2;
+let taghsim=number1/number2;
+
+
+alert('حاصل جمع '+plus);
+alert('حاصل تفریق '+mines);
+alert('حاصل ضرب '+zarb);
+alert('حاصل تقسیم '+taghsim);
